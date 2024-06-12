e5c5d3aab7d6861d6e30c1fe5068f98ac0dc2633
PranitAdgokar <136226664+PranitAdgokar@users.noreply.github.com>
2024-06-12 18:20:27 +0530

    chore: Update signup page layout and styles

--- src/Auth/SignupPage.js
+++ src/Auth/SignupPage.js
@@ -6,27 +6,27 @@ import { Link } from "react-router-dom";
 
 export function SignupPage() {
   return (
-    <div className="flex justify-center mx-auto">
-      <div className="glass-morphi container-box w-4/6 h-3/5 rounded-3xl flex justify-between flex-start ">
-        <div className="w-2/6">
+    <div className="container flex justify-center mx-auto items-center">
+      <div className="glass-morphi grid grid-cols-2 rounded-3xl  ">
+        <div className="w-full flex justify-center">
           <Link to="/">
             <img
-              className="w-8 h-8 mt-7 ml-10"
+              className="w-8 h-8 mt-4"
               src="/Images/icons-left-64.png"
               alt="back"
             />
           </Link>
           <img
-            className="h-auto max-w-full ml-8 mb-8"
+            className="h-auto max-w-[350px]"
             src="/Images/reg-png.png"
             alt="Register"
           />
         </div>
-        <form className=" flex w-2/3 h-full gap-8 mr-8 ">
+        <form className="grid grid-cols-1 sm:grid-cols-2 md:">
           {/* <h2 className="ml-36 mt-3 font-sans font-extrabold text-lg ">
             Log in
           </h2> */}
-          <div className=" w-1/2 flex flex-col gap-2">
+          <div className="px-8">
             <h2 className=" mt-3 font-sans font-extrabold text-lg ">Sign Up</h2>
             <label className="font-bold" htmlfor="username">
               Username
@@ -37,11 +37,14 @@ export function SignupPage() {
               Password
             </label>
             <Input type="password" placeholder="Password" id="password" />
-            <Link to="/" className="mt-10 text-red-500 ml-10">
-              Already have Account? log in
+            <Link to="/" className="text-gray-300">
+              Already have Account?{" "}
+              <span className=" text-white">
+                <b>log in</b>
+              </span>
             </Link>
           </div>
-          <div className="w-1/2 mt-12 flex flex-col gap-2 ">
+          <div className="px-8 mt-10">
             <label className="font-bold" htmlfor="name">
               Name
             </label>
@@ -55,7 +58,7 @@ export function SignupPage() {
               placeholder=" Confirm Password"
               id=" conf-password"
             />
-            <ShimmerButton className="shadow-2xl mt-4">
+            <ShimmerButton className="shadow-2xl mt-6">
               <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
                 Register
               </span>
